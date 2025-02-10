ed4387d4ee73c982276ced5065179ba63c3be7f0
CarlosMelgar <164534523+Carlos-Melgar-1079@users.noreply.github.com>
2025-02-10 18:32:22 +0000

    publique funcion rama

---
+++ funcionflecha.dart
@@ -0,0 +1,27 @@
+int multiplica(int a, int b){
+return a * b;
+}
+int flechamultiplica(int a, int b) => a * b;
+//returnType functionName(parameters...) => expression;
+
+void main() {
+  print('carlos Melgar a.22308051281079');
+  print('Llamar a la función multiplica');
+  print('La multiplicación de 10 * 5 es: ${multiplica(10, 5)}');
+  print('Llamar a la función flechamultiplica');
+  print(flechamultiplica(3, 5));
+} 
+
+double divicion (double a, double b){
+  return a / b;
+}
+
+double flechadivicion (double a, double b) => a / b;
+
+void main() {
+  print('carlos Melgar a.22308051281079');
+  print('Llamar a la función divicion');
+  print('La divicion de 10 / 5 es: ${divicion(10, 5)}');  
+  print('Llamar a la función flechadivicion');
+  print(flechadivicion(10, 5));
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -1,2 +1,5 @@
 ejemplo de salidas
 ![alt text](image.png)
+
+EJEMPLO DE SALIDA funcion normal + funcion flecha
+![alt text](image-1.png)
